dc13461853688d80b2c3543b3decd0531963cb09
Sivaram <notifysiva@gmail.com>
2024-11-11 12:34:06 +0530

    commit 11-11 1

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -6,5 +6,9 @@ export const createPost = async (req, res) => {
     let { img } = req.body;
     const userId = req.user._id.toString();
     const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    
   } catch (err) {}
 };
