710909e12777029e30db79eac026337b4f400d26
Sivaram <notifysiva@gmail.com>
2024-11-12 17:53:18 +0530

    patch update 2

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import { connectMongoDB } from "./db/connectMongoDB.js";
