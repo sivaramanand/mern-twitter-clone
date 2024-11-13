247d57f21671c4a31becf20a19d28ae2cfe9234f
Sivaram <notifysiva@gmail.com>
2024-11-13 16:17:50 +0530

    post commit 6

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use("/api/posts", postRoutes);
 app.use("/api/routes", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
